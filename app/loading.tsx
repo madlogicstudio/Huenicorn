2b3775b4ee2eedf958b1602b263faf30f6da9238
Jutsy <madlogicstudiox@gmail.com>
2026-09-22 13:45:31 +0800

    Added loading screen

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ import { TwinOrbit } from "@/components/ui/TwinOrbit";
 export default function loading() {
     return (
         <div className="flex min-h-screen items-center justify-center">
-            <TwinOrbit className="lg:size-6 size-3 text-[var(--primary)]" />
+            <TwinOrbit className="size-4 text-[var(--primary)]" />
         </div>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,23 @@ import What from "@/components/landing/What"
 import Why from "@/components/landing/Why"
 import CTA from "@/components/landing/Cta"
 import Footer from "@/components/landing/Footer"
+import { useEffect, useState } from "react"
+import Loading from "./loading"
 
 function page() {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000)
+  }, [])
+
+  if (isLoading) return (
+    <Loading />
+  )
+
   return (
     <div className="w-full flex flex-col items-center">
       <Header />
@@ -16,7 +31,7 @@ function page() {
       <Why />
       <CTA />
       <Footer />
-      
+
     </div>
   )
 }
